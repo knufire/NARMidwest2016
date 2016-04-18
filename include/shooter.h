56757e848297c0a220430550057e97fcef200e56
Rahul <knufire@gmail.com>
2016-04-18 01:48:51 -0400

    Changed shooter target RPMs

--- include/shooter.h
+++ include/shooter.h
@@ -15,7 +15,7 @@ void runShooter();
 void setShooterRPM(int rpm);
 
 #define SHOOTER_SPEED_SHORT			1000	// Shooter RPM for close shot
-#define SHOOTER_SPEED_MID			3200	// Shooter RPM for mid shot
+#define SHOOTER_SPEED_MID			2800	// Shooter RPM for mid shot
 #define SHOOTER_SPEED_LONG			4000	// Shooter RPM for far/fullcourt shot
 
 #endif /* INCLUDE_SHOOTER_H_ */
